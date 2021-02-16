5833f26aef746157fa09c0fb4dd7fa41cac5e8da
mehmetbltt <mehmetbltt@gmail.com>
2021-02-16 17:16:44 +0300

    schema folder rename

--- src/Request/RequestBase.php
+++ src/Request/RequestBase.php
@@ -68,7 +68,7 @@ class RequestBase implements \JsonSerializable
 
 	public function getSchemaFile()
 	{
-		$local_path = 'schema/'.$this->schemaJsonFileName;
+		$local_path = 'schemas/'.$this->schemaJsonFileName;
 		if(!realpath($local_path)) {
 			file_put_contents($local_path,file_get_contents($this->schema));
 		}
